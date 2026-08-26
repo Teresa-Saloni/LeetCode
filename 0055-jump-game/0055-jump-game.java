66d6e0db834026272c8a66a81643ac47fcb063a8
Saloni <teresasaloni09@gmail.com>
2026-08-26 11:07:39 +0530

    Time: 2 ms (89.07%), Space: 47.3 MB (98.59%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int farthest = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i > farthest) return false;
-            farthest = Math.max(farthest, i + nums[i]);
+        int maxReach = 0;
+        for(int i = 0; i < nums.length;i++){
+            if(i > maxReach){
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if(maxReach > nums.length -1 ) return true;
         }
         return true;
     }
