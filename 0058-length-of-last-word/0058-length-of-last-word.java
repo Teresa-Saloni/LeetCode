1d4c78cd6d44bb3c4dae4dd94c0cb64aa8e95c55
Saloni <teresasaloni09@gmail.com>
2026-08-27 12:36:10 +0530

    Time: 0 ms (100%), Space: 43.3 MB (16.1%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+
+        // skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // count the last word
+        int length = 0;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        return length;
+    }
+}
