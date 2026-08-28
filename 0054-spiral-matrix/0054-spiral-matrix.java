73ff3501c2e0e53d13814fad579dd4f63a8a0c47
Saloni <teresasaloni09@gmail.com>
2026-08-28 10:30:33 +0530

    Time: 0 ms (100%), Space: 42.9 MB (80.19%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int left = 0, right = matrix[0].length - 1,top = 0,bottom = matrix.length -1;
+        while(top <= bottom && left <= right){
+            for(int i = left; i <= right;i++){
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for(int j = top ; j <= bottom ; j++){
+                ans.add(matrix[j][right]);
+            }
+            right--;
+            if(top <= bottom){
+                for(int i = right; i >= left;i--){
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left <= right){
+                for(int i = bottom; i >= top; i-- ){
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+}
