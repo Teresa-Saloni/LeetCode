12e337a3d0d198fccf57a92c4a4f898cf81789f5
Saloni <teresasaloni09@gmail.com>
2026-09-20 20:41:06 +0530

    Time: 0 ms (100%), Space: 43.9 MB (49.61%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode smallDummy = new ListNode(0);
+        ListNode largeDummy = new ListNode(0);
+
+        ListNode small = smallDummy;
+        ListNode large = largeDummy;
+
+        while (head != null) {
+
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+
+            head = head.next;
+        }
+
+        // Important: terminate large list
+        large.next = null;
+
+        // Connect both lists
+        small.next = largeDummy.next;
+
+        return smallDummy.next;
+    }
+}
