89ee9185c64f2251eb14810e6a0aecefc7fc1d76
Saloni <teresasaloni09@gmail.com>
2026-09-25 23:57:27 +0530

    Time: 0 ms (100%), Space: 43.8 MB (32.09%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        dfs(root, 0, ans);
+        return ans;
+    }
+
+    private void dfs(TreeNode node, int depth, List<Integer> ans) {
+        if (node == null) return;
+
+        // First node visited at this depth is the rightmost node
+        if (depth == ans.size()) {
+            ans.add(node.val);
+        }
+
+        // Visit right first
+        dfs(node.right, depth + 1, ans);
+        dfs(node.left, depth + 1, ans);
+    }
+}
